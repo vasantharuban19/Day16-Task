53fe7c3e06d70f4b0da4997d36690906805bcb01
vasantharuban19 <vasanthruban1920@gmail.com>
2023-11-27 11:58:43 +0530

    Day16 task completed

---
+++ README.md
@@ -0,0 +1,1 @@
+In this task we have achieved the callback hell using setTimeout.
---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  document.getElementById("display").innerText = "10";
+  setTimeout(() => {
+    document.getElementById("display").innerText = "9";
+    setTimeout(() => {
+      document.getElementById("display").innerText = "8";
+      setTimeout(() => {
+        document.getElementById("display").innerText = "7";
+        setTimeout(() => {
+          document.getElementById("display").innerText = "6";
+          setTimeout(() => {
+            document.getElementById("display").innerText = "5";
+            setTimeout(() => {
+              document.getElementById("display").innerText = "4";
+              setTimeout(() => {
+                document.getElementById("display").innerText = "3";
+                setTimeout(() => {
+                  document.getElementById("display").innerText = "2";
+                  setTimeout(() => {
+                    document.getElementById("display").innerText = "1";
+                    setTimeout(() => {
+                      document.getElementById("display").innerText =
+                        "Happy Independence Day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
